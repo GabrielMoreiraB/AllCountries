25b14f157ae6656897315a18e6e1fc3517aedc5f
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-05-08 17:57:27 -0300

    filter countries

--- src/pages/ReactCountriesPage.jsx
+++ src/pages/ReactCountriesPage.jsx
@@ -1,14 +1,38 @@
+import { useState } from 'react';
 import Header from '../components/Header';
 import Main from '../components/Main';
+import TextInput from '../components/TextInput';
 import { allCountries } from '../data/countries';
 
 const ReactCountries = () => {
-    console.log(allCountries)
+    const [coutryFilter, setCountryFilter] = useState('Brazil');
+
+    function handleCountryFilterChange(e){
+      setCountryFilter(e)
+    }
+
+    const coutryFilterLowercase = coutryFilter.toLocaleLowerCase();
+
+    const filteredCountries = 
+    coutryFilterLowercase.length >= 3 ?
+    allCountries.filter(country=> country.nameLowerCase
+      .includes(coutryFilterLowercase)) : allCountries;
+
+    console.log(filteredCountries)
+
     return ( 
         <div>
         <Header>React-Countries</Header>
   
-        <Main>Teste2</Main>
+        <Main>
+          <TextInput
+          id='inputCountryChange'
+          labelDescription='Informe o Nome do País (Pelo menos 3 caracteres):'
+          inputValue={coutryFilter}
+          autoFocus
+          onInputChange={handleCountryFilterChange}
+          />
+        </Main>
         
       </div>
      );
