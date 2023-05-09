1e24b6733b9434a401dbd219688ca1096b8f1438
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-05-09 14:26:14 -0300

    .

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -4,8 +4,14 @@
 const Country = ({children:country = null}) => {
     if(!country) <div>Impossível renderizar país</div>
     return (
-        <div>
-            {country.name}
+        <div className="border p-2 m-2">
+            
+            <ul>
+                <li>{country.name}</li>
+                <li>{country.capital}</li>
+                <li>{country.region}</li>
+                <li>{country.area} km²</li>
+            </ul>
         </div>
     )
 }
